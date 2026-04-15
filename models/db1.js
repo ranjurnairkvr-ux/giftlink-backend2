461d9b0503fa5a61d9ef8d1822df450430c5d838
ranjurnairkvr-ux <ranjurnairkvr@gmail.com>
2026-04-15 10:58:17 +0530

    Create db1.js

---
+++ models/db1.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db("mydatabase");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+module.exports = connectToDatabase;
