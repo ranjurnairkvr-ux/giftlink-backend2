1ce28907abff4c5861f3ea2963cffe39e7f762f6
ranjurnairkvr-ux <ranjurnairkvr@gmail.com>
2026-04-15 11:23:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const app = express();
 
-const giftRoutes = require("./routes/giftRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 
-app.use(express.json());
-
-
-app.use("/api/gifts", giftRoutes);
-
+// This line serves /api/search
 app.use("/api/search", searchRoutes);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
